2122ac94a6efbe187fc063573a8c87396e7575fa
SAGAR T <ssagu6661@gmail.com>
2024-08-10 13:17:09 -0400

    Add files via upload

---
+++ c7.py
@@ -0,0 +1,13 @@
+print("my name is sagar")
+name=input("what is your name ")
+age = input("what is your age  ")
+age2 = int(input("sagar what is your age   "))
+print("had a dinner",name)
+print("yes i had dinner")
+print("what about you sagar")
+print("yes i had dineer")
+print("now where are you", name)
+print("iam at bangalore")
+
+
+
